82a14fd34d2b4fa4cd8fcdb80bc74185133f59eb
MissDitch <mjtsloot@icloud.com>
2017-06-15 16:32:32 +0200

    all elements in place

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function init() {   
   // listener("add", "id", "onOff", onAndOff); // uncomment this if on-off switch should function
-   listener("add", "id", "start", start); // comment this out if on-off switch should function
-   listener("add", "id", "strict", strict); // comment this out if on-off switch should function
+   listener("add", "id", "start", "click", start); // comment this out if on-off switch should function
+   listener("add", "id", "reset", "click", reset); // comment this out if on-off switch should function
+   listener("add", "id", "strict", "click", strict); // comment this out if on-off switch should function
 }
 var greenAudio = document.createElement('audio'),
     redAudio = document.createElement('audio'),
@@ -14,19 +15,19 @@ var greenAudio = document.createElement('audio'),
     blueAudio.setAttribute('src','https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
 
-function listener(addOrRemove, prop, selector, func) {
+function listener(addOrRemove, prop, selector, event, func) {
     var op = addOrRemove;
     if (prop === "class") {
         var sels = document.querySelectorAll(selector);
         for (var i = 0; i < sels.length; i++) {
-            if (op === "add") { sels[i].addEventListener("click", func); }
-            else if (op === "remove") { sels[i].removeEventListener("click", func); }
+            if (op === "add") { sels[i].addEventListener(event, func); }
+            else if (op === "remove") { sels[i].removeEventListener(event, func); }
         }
   }
   if (prop === "id") {
       var el = document.getElementById(selector);
-      if (op === "add") {  el.addEventListener("click", func); }
-      else if (op === "remove") { el.removeEventListener("click", func); }
+      if (op === "add") {  el.addEventListener(event, func); }
+      else if (op === "remove") { el.removeEventListener(event, func); }
   }
 }
 function onAndOff(e) {
@@ -41,31 +42,36 @@ function onAndOff(e) {
         //  alert("on");
         e.target.classList.add("onOff");     
         //activate start button
-        listener("add", "id", "start", start);
+        listener("add", "id", "start", "click", start);
+        // activate reset button
+         listener("add", "id", "reset", "click", reset); 
         //activate strict button
-        listener("add", "id", "strict", strict);    
+        listener("add", "id", "strict", "click", strict);    
     }
 }
 function start(e) {
-    e.target.style = "background:#42A5F5";
-    listener("add", "id", "green", play);    
-    listener("add", "id", "red", play);
-    listener("add", "id", "blue", play);
-    listener("add", "id", "yellow", play);
+   // e.target.style = "background:#42A5F5";
+    listener("add", "id", "green", "click", play);    
+    listener("add", "id", "red", "click", play);
+    listener("add", "id", "blue", "click", play);
+    listener("add", "id", "yellow", "click", play);
     
     document.getElementById("strict").style = "background:rgba(255,255,255, 0.5)";
 }
 function strict(e) {
     e.target.style = "background:#42A5F5";
-    document.getElementById("start").style = "background:rgba(255,255,255, 0.5)";
+   // document.getElementById("start").style = "background:rgba(255,255,255, 0.5)";
 
 }
+function reset(e) {
+     document.getElementById("strict").style = "background:rgba(255,255,255, 0.5)";
+}
 function stop() {
-    listener("remove", "id", "start", start);
-    listener("remove", "id", "green", play);    
-    listener("remove", "id", "red", play);
-    listener("remove", "id", "blue", play);
-    listener("remove", "id", "yellow", play);
+    listener("remove", "id", "start", "click", start);
+    listener("remove", "id", "green", "click", play);    
+    listener("remove", "id", "red", "click", play);
+    listener("remove", "id", "blue", "click", play);
+    listener("remove", "id", "yellow", "click", play);
 }
 function play(e) {
     var audio, target, origStyle, playStyle;
